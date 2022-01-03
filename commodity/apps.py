50dd5e629c6a032440ceb65d67731a51d6b852b6
qq50722508 <50722508@qq.com>
2022-01-03 14:41:15 +0800

    。。

--- commodity/apps.py
+++ commodity/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class CommodityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'commodity'
--- shopper/apps.py
+++ shopper/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class ShooperConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class ShopperConfig(AppConfig):
     name = 'shopper'
